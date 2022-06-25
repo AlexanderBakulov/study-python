6186ba4c368b4735e696825f6e82d372ddb598d0
Alexander Bakulov <asbakulov@gmail.com>
2022-06-25 16:04:13 +0600

    5.2

--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -30,21 +30,22 @@ Out[1]: '11111111111111111111111111110000'
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
-prefix = input('Input ip and mask (example 10.1.1.0/24): ')
+prefix = input('Input ip and mask (example 10.1.1.12/24): ')
 prefix_list = prefix.split('.')
 mask = prefix_list[3][-3:]
-prefix_list[3] = prefix_list[3][:-3]
+prefix_list[3] = '0'
 ip_addr_1 = f'{int(prefix_list[0]):<10}{int(prefix_list[1]):<10}{int(prefix_list[2]):<10}{int(prefix_list[3]):<10}'
 ip_addr_2 = '{:08b}  {:08b}  {:08b}  {:08b}'.format(int(prefix_list[0]), int(prefix_list[1]), int(prefix_list[2]), int(prefix_list[3]))
 mask_bin = '1'*int(mask[1:]) + '0'*(32 - int(mask[1:]))
+mask_dec = f'{int(mask_bin[0:8],2):<10}{int(mask_bin[8:16], 2):<10}{int(mask_bin[16:24], 2):<10}{int(mask_bin[24:32], 2):<10}'
+mask_bits = f'{mask_bin[0:8]}  {mask_bin[8:16]}  {mask_bin[16:24]}  {mask_bin[24:32]}'
 
 
 
-
-print('Ip address:')
+print('Network:')
 print(ip_addr_1)
 print(ip_addr_2 + '\n')
 print('Mask:')
-#print(int(prefix_list[4]))
 print(mask)
-print(mask_bin)
+print(mask_dec)
+print(mask_bits)
